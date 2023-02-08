0ac959266550bec3e71544d4b2f56bf5f604ba1d
Hô Văn Toàn <it.vantoan@gmail.com>
2023-02-08 15:08:32 +0700

    Feature: Add Job Auto delete invitation after thirty day

---
+++ Minder.Service/Jobs/ExpireInvitation.cs
@@ -0,0 +1,25 @@
+﻿using Coravel.Invocable;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Minder.Database;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minder.Service.Jobs {
+
+    public class ExpireInvitation : IInvocable {
+        private readonly MinderContext db;
+
+        public ExpireInvitation(IServiceProvider serviceProvider) {
+            this.db = serviceProvider.GetRequiredService<MinderContext>();
+        }
+
+        public async Task Invoke() {
+            var invitations = await this.db.Invites.Where(o => (DateTimeOffset.Now - o.CreateAt).TotalDays > 30).ToListAsync();
+
+            this.db.RemoveRange(invitations);
+            await this.db.SaveChangesAsync();
+        }
+    }
+}
