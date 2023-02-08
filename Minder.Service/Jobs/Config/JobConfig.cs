794fbdfdb24fa181dc0f4a4eb37ff175a78348dd
Hô Văn Toàn <it.vantoan@gmail.com>
2023-02-08 15:08:32 +0700

    Feature: Add Job Auto delete invitation after thirty day

---
+++ Minder.Service/Jobs/Config/JobConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Minder.Service.Jobs.Config {
+
+    public class JobConfig {
+        public string Name { get; set; } = null!;
+        public int BatchWrite { get; set; } = 1000;
+    }
+}
